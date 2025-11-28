60b757af4db290f5cbbb4c4b5d00e0ab30fd912c
juan <130606010+juancastrop@users.noreply.github.com>
2025-11-28 23:03:28 +0000

    creacion de tablas y sus relaciones

--- src/models.py
+++ src/models.py
@@ -1,19 +1,95 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Column, ForeignKey, Table
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
+
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    lastname: Mapped[str] = mapped_column(String(200), nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "lastname": self.name,
             # do not serialize the password, its a security breach
         }
+
+
+class planet(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    rotation_period: Mapped[int] = mapped_column(nullable=False)
+    orbital_period: Mapped[int] = mapped_column(nullable=False)
+    diameter: Mapped[int] = mapped_column(nullable=False)
+    climate: Mapped[str] = mapped_column(nullable=False)
+    gravity: Mapped[str] = mapped_column(nullable=False)
+    terrain: Mapped[str] = mapped_column(nullable=False)
+    surface_water: Mapped[str] = mapped_column(nullable=False)
+    population: Mapped[str] = mapped_column(nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period,
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity,
+            "terrain": self.terrain,
+            "surface_water": self.surface_water,
+            "population": self.population,
+
+        }
+
+
+class character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(200), nullable=False)
+    height: Mapped[int] = mapped_column(nullable=False)
+    mass: Mapped[int] = mapped_column(nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(100), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(50), nullable=False)
+
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+
+
+        }
+
+
+fav_planets = Table(
+    "fav_planets",
+    db.metadata,
+    Column("fav_user_id", ForeignKey("user.id")),
+    Column("fav_planet_id", ForeignKey("planet.id"))
+)
+
+fav_characters = Table(
+    "fav_characters",
+    db.metadata,
+    Column("fav_user_id", ForeignKey("user.id")),
+    Column("fav_character_id", ForeignKey("character.id"))
+)
